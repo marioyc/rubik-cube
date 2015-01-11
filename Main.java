0a40ba5c3928cbba37edfabc4b6ac34483345bea
marioyc <ycmario@gmail.com>
2015-01-11 15:30:11 +0100

    use IDASolverReturn

--- Main.java
+++ Main.java
@@ -8,13 +8,12 @@ public class Main{
 	// YWYYGGYBBGBWWRWWOBGGWBBYOOWOGGROBBOBRYGRWRORROGYOYYRWR 7
 	// OYBRGRRGRYRWBRYBRYROOGBBGBYGGBWOWOYWWWWOWWOYBYGROYOGBG 7
 	// GGWGGYYYOBRGORGYOWOBYOBYBRWBOWBOWGWRRWRRWROWYBBRGYYGBO 7
-	static int testIDA(IDASolverInterface solver, int moves){
+	static IDASolverReturn testIDA(IDASolverInterface solver, int moves){
 		Cube C = new Cube();
 		C.mixCube(moves);
 		System.out.println(C);
 		
-		int ret = solver.solve(C);
-		return ret;
+		return solver.solve(C);
 	}
 	
 	
@@ -30,27 +29,25 @@ public class Main{
 			System.out.println(":(");
 		}
 		
-		long startTime = System.nanoTime();
-		int ret1 = solver1.solve(C);
-		long endTime = System.nanoTime();
-		double duration = (endTime - startTime) / 1000000.0;
+		IDASolverReturn ret1 = solver1.solve(C);
 		System.out.println("IDA1 : " + ret1);
-		System.out.println("duration1 = " + duration + " milliseconds");
 		
-		startTime = System.nanoTime();
-		int ret2 = solver2.solve(C2);
-		endTime = System.nanoTime();
-		duration = (endTime - startTime) / 1000000.0;
+		IDASolverReturn ret2 = solver2.solve(C2);
 		System.out.println("IDA2 : " + ret2);
-		System.out.println("duration2 = " + duration + " milliseconds");
+		System.out.println("-------------------------------------------\n");
 	}
 	
     public static void main(String args[]){
     	IDA1 solver1 = new IDA1();
     	IDA2 solver2 = new IDA2();
     	
-    	for(int i = 0;i < 15;++i)
-	    	compareIDA(solver1,solver2,7);
+    	for(int i = 0;i < 15;++i){
+    		IDASolverReturn ret = testIDA(solver2,9);
+    		System.out.println(ret);
+    	}
+    	
+    	//for(int i = 0;i < 15;++i)
+	    //	compareIDA(solver1,solver2,7);
     	
     	/*
 		double total[] = new double[10];
